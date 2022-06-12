3fb245aaed52a0357efb570c036dfffc33b55ceb
Chetan <vipershiraguppi@gmail.com>
2022-06-12 09:56:15 +0530

    truffle integrated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-// import web3 from 'web3';
-
+import Web3 from 'web3'
+import { useState } from 'react';
 function App() {
+async function loadData(){
+  const web3=new Web3(Web3.givenProvider || "http://localhost:8545")
+  const network= await web3.eth.net.getNetworkType();
+  setAccount(await web3.eth.getAccounts())
+} 
+const [account,setAccount]=useState(null);
+
   return (
     <div className="container">
       <h1>asdf asdf asd fasd fasdf</h1>
+      <p>Account: {account}</p>
+      <button onClick={loadData}>hii</button>
     </div>
   );
 }
