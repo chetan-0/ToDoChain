508f2d823d3fd347f3aaa9824cb897221897a82b
Chetan <vipershiraguppi@gmail.com>
2022-06-12 10:25:34 +0530

    tasks smart contract added

--- contracts/DoList.sol
+++ contracts/DoList.sol
@@ -1,4 +1,20 @@
 pragma solidity ^0.5.0;
 contract ToDoList{
-    uint public tasks=0;
+    uint public taskCount=0;
+    mapping(uint => Task) public tasks;
+    struct Task{
+        uint id;
+        string content;
+        bool status;
+    }
+    constructor() public {
+        addtask("Do laundary at 1300");
+    }
+    function addtask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount]=Task(taskCount,_content,false);
+    }
+
+
+
 }
